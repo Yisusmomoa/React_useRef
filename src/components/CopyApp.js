c6f2e1e76c7616fe0ae88eb9aa5b0d7363b8e423
Brandon Loera Silva <bjls29@hotmail.com>
2022-08-08 01:31:40 -0500

    add CopyApp component

--- src/components/CopyApp.js
+++ src/components/CopyApp.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { useState, useRef } from "react";
+import Swal from 'sweetalert2'
 
 const CopyApp = () => {
     const [text, setText]=useState('');
     const copyRef=useRef();
+
     const handleCopy=()=>{
         const copyInput=copyRef.current;
         //console.log(copyInput.value)
         copyInput.select();//seleciona el texto/value del input
         document.execCommand('copy');
+        Swal.fire('Text copied')
     }
+
     return (
         <div>
             <input type='text' value={text} ref={copyRef} onChange={(e)=>setText(e.target.value)}/>
             <button onClick={handleCopy}>Copy input</button>
         </div>
     );
+    
 }
 
 export default CopyApp;
+
+
